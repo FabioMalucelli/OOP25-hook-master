36a298ba36f9862b64008910c8fcc6fbbeb46734
Mattia Evangelisti <mattia.evangelisti@studio.unibo.it>
2026-07-23 17:28:02 +0200

    fix

--- src/main/java/it/unibo/hookmaster/model/fishdata/FishManager.java
+++ src/main/java/it/unibo/hookmaster/model/fishdata/FishManager.java
@@ -160,7 +160,7 @@ public class FishManager implements WeatherObserver {
                     this.boidsManager.spawnBoids();
                     this.boidsSpawnPending = false;
                 } else {
-                    break;
+                    spawnFish();
                 }
             } else {
                 spawnFish();
